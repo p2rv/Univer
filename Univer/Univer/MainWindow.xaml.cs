9ceae829f06f5156100a5d15fd10593bd2fb27e9
Pavel_iMAC <pros_pavlo@mail.ru>
2015-06-06 14:16:50 +1000

    First initial commit

---
+++ Univer/Univer/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Univer
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    struct Specialty
+    {
+        public string FullName { get; set; }
+        public string SpecialtyCode { get; set; }
+    }
+    struct Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+    struct Student
+    {
+        private Person person;
+        public string FirstName
+        {
+            get { return person.FirstName; }
+            set { person.FirstName = value; }
+        }
+        public string LastName
+        {
+            get { return person.LastName; }
+            set { person.LastName = value; }
+        }
+    }
+    struct Teacher
+    {
+        private Person person;
+        public string FirstName
+        {
+            get { return person.FirstName; }
+            set { person.FirstName = value; }
+        }
+        public string LastName
+        {
+            get { return person.LastName; }
+            set { person.LastName = value; }
+        }
+    }
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
